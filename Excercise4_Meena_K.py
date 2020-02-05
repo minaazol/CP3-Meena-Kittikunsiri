35f02e64c3068cc33bc28ebd656e5eb053411f2e
Minaa <minaazol@gmail.com>
2020-02-05 23:15:40 +0700

    Create Excercise4_Meena_K.py

---
+++ Excercise4_Meena_K.py
@@ -0,0 +1,10 @@
+FE = 87
+GB = 99
+CS = 97
+CP = 100
+print("--- Your Score ---")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ", CS)
+print("Computer Programming : ", CP)
